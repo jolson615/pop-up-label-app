082528ad6b9fe311815494876ad880fe3bf8a779
jolson615 <jolson615@gmail.com>
2017-07-13 17:59:26 -0400

    Initial Commit

---
+++ PopUps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopUps
+//
+//  Created by Jeffrey L Olson Jr on 7/13/17.
+//  Copyright © 2017 Jeffrey L Olson Jr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
